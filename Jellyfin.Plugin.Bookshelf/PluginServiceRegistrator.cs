1b3da9495422b75a264f14018a48b032777f4911
dkanada <dkanada@users.noreply.github.com>
2025-12-07 13:18:28 +0900

    remove local metadata and image providers

--- Jellyfin.Plugin.Bookshelf/PluginServiceRegistrator.cs
+++ Jellyfin.Plugin.Bookshelf/PluginServiceRegistrator.cs
@@ -1,6 +1,3 @@
-using Jellyfin.Plugin.Bookshelf.Providers;
-using Jellyfin.Plugin.Bookshelf.Providers.ComicBookInfo;
-using Jellyfin.Plugin.Bookshelf.Providers.ComicInfo;
 using Jellyfin.Plugin.Bookshelf.Providers.ComicVine;
 using MediaBrowser.Controller;
 using MediaBrowser.Controller.Plugins;
@@ -16,14 +13,6 @@ namespace Jellyfin.Plugin.Bookshelf
         /// <inheritdoc />
         public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
         {
-            // register the proxy local metadata provider for comic files
-            serviceCollection.AddSingleton<ComicFileProvider>();
-
-            // register the actual implementations of the local metadata provider for comic files
-            serviceCollection.AddSingleton<IComicFileProvider, ExternalComicInfoProvider>();
-            serviceCollection.AddSingleton<IComicFileProvider, InternalComicInfoProvider>();
-            serviceCollection.AddSingleton<IComicFileProvider, ComicBookInfoProvider>();
-
             serviceCollection.AddSingleton<IComicVineMetadataCacheManager, ComicVineMetadataCacheManager>();
             serviceCollection.AddSingleton<IComicVineApiKeyProvider, ComicVineApiKeyProvider>();
         }
